f771111fbdff5b07bb92a53a7e0cab75e3239e78
Akash Sooraj <akashsooraj6493@gmail.com>
2021-07-01 18:29:40 +0530

    go fmt stack package

--- stack/stack.go
+++ stack/stack.go
@@ -6,102 +6,102 @@ import "fmt"
 
 // Private Node for the singly linked list
 type node struct {
-    data interface{}
-    next *node
+	data interface{}
+	next *node
 }
 
 // Struct for the Stack data type
 type Stack struct {
-    head *node
-    size int
+	head *node
+	size int
 }
 
 // New creates and returns a new empty stack.
 func New() Stack {
-    return Stack {
-        head: nil,
-        size: 0,
-    }
+	return Stack{
+		head: nil,
+		size: 0,
+	}
 }
 
 // Size returns the number of elements in the stack currently.
 func (s *Stack) Size() int {
-    return s.size
+	return s.size
 }
 
 // Push takes any number of arguments (variadic function) and adds each one
 // of them to the stack top. The last argument supplied is at the top after
 // the Push operation. Returns true on success and false on failure
 func (s *Stack) Push(vals ...interface{}) bool {
-    if s == nil {
-        return false
-    }
+	if s == nil {
+		return false
+	}
 
-    for _, val := range vals {
-        // for each val, create a new node
-        // out of the val
-        newNode := node {
-            data: val,
-            next: s.head,
-        }
-        
-        s.head = &newNode
-        s.size++
-    }
-    return true
+	for _, val := range vals {
+		// for each val, create a new node
+		// out of the val
+		newNode := node{
+			data: val,
+			next: s.head,
+		}
+
+		s.head = &newNode
+		s.size++
+	}
+	return true
 }
 
 // String allows to print the stack. Can be used with fmt.Print or the likes.
 func (s Stack) String() string {
-    res := ""
-    sep := " "
-    for curr := s.head; curr != nil; curr = curr.next {
-        if curr.next == nil {
-            sep = ""
-        }
-        res += fmt.Sprintf("%v%v", curr.data, sep);
-    }
-    return res
+	res := ""
+	sep := " "
+	for curr := s.head; curr != nil; curr = curr.next {
+		if curr.next == nil {
+			sep = ""
+		}
+		res += fmt.Sprintf("%v%v", curr.data, sep)
+	}
+	return res
 }
 
 // Empty returns if a stack is empty. Returns true if stack has 0 elements
 // and false otherwise
 func (s *Stack) Empty() bool {
-    if s == nil || s.size == 0 {
-        return true
-    }
-    return false
+	if s == nil || s.size == 0 {
+		return true
+	}
+	return false
 }
 
 // Pop returns the top of the Stack and removes it from the stack, if it exists.
 // Returns the data and ok (bool). If the top exists, ok = true. False otherwise.
 func (s *Stack) Pop() (interface{}, bool) {
-    if s == nil || s.Empty() {
-        return nil, false
-    }
+	if s == nil || s.Empty() {
+		return nil, false
+	}
 
-    oldHead := s.head
-    s.head = oldHead.next
-    s.size--
-    return oldHead.data, true
+	oldHead := s.head
+	s.head = oldHead.next
+	s.size--
+	return oldHead.data, true
 }
 
 // Clear resets the stack.
 func (s *Stack) Clear() {
-    if s != nil {
-        // let the GC do all the work
-        s.head = nil
-        s.size = 0
-    }
+	if s != nil {
+		// let the GC do all the work
+		s.head = nil
+		s.size = 0
+	}
 }
 
 // Peek returns the top of the stack without popping off, if it exists.
 // Returns a pointer to the data and a bool(ok), which denotes successful query of the top.
 // If the top exists, ok = true. False otherwise
 func (s *Stack) Peek() (*interface{}, bool) {
-    if s == nil || s.Empty() {
-        return nil, false
-    }
+	if s == nil || s.Empty() {
+		return nil, false
+	}
 
-    return &s.head.data, true
+	return &s.head.data, true
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,80 +1,80 @@
 package stack
 
 import (
-    "testing"
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
-    s := New()
-    if s.size != 0 {
-        t.Errorf("New: size test: got %v, wanted 0", s.size)
-    }
+	s := New()
+	if s.size != 0 {
+		t.Errorf("New: size test: got %v, wanted 0", s.size)
+	}
 
-    if s.head != nil {
-        t.Errorf("New: head test: got non-empty head")
-    }
+	if s.head != nil {
+		t.Errorf("New: head test: got non-empty head")
+	}
 }
 
 func TestPush(t *testing.T) {
-    s := New()
-    vals := []interface{}{1, 2, 3, 4, 5}
-    s.Push(vals...)
-    fmt.Println(s.String())
+	s := New()
+	vals := []interface{}{1, 2, 3, 4, 5}
+	s.Push(vals...)
+	fmt.Println(s.String())
 
-    want := 5
-    got := s.Size()
-    if got != want {
-        t.Errorf("Push: Size: got %v, wanted %v", got, want)
-    }
+	want := 5
+	got := s.Size()
+	if got != want {
+		t.Errorf("Push: Size: got %v, wanted %v", got, want)
+	}
 }
 
 func TestPop(t *testing.T) {
-    s := New()
-    n := 20
-    rand.Seed(time.Time{}.Unix())
-    vals := make([]interface{}, 0, n)
-    for i := 0; i < n; i++ {
-        vals = append(vals, rand.Int())
-    }
-    s.Push(vals...)
-    fmt.Println("TestPop: Size: ", s.Size())
+	s := New()
+	n := 20
+	rand.Seed(time.Time{}.Unix())
+	vals := make([]interface{}, 0, n)
+	for i := 0; i < n; i++ {
+		vals = append(vals, rand.Int())
+	}
+	s.Push(vals...)
+	fmt.Println("TestPop: Size: ", s.Size())
 
-    for i := n - 1; i >= 0; i-- {
-        data, ok := s.Pop()
-        if !ok {
-            t.Errorf("Pop: Spurious false value from non-empty stack")
-        }
-        got := data.(int)
-        want := vals[i].(int)
-        if got != want {
-            t.Errorf("Pop: got %v, wanted %v", got, want)
-        }
-    }
+	for i := n - 1; i >= 0; i-- {
+		data, ok := s.Pop()
+		if !ok {
+			t.Errorf("Pop: Spurious false value from non-empty stack")
+		}
+		got := data.(int)
+		want := vals[i].(int)
+		if got != want {
+			t.Errorf("Pop: got %v, wanted %v", got, want)
+		}
+	}
 
-    if !s.Empty() {
-        t.Errorf("Pop: non-empty stack after all pops")
-    }
+	if !s.Empty() {
+		t.Errorf("Pop: non-empty stack after all pops")
+	}
 }
 
 func TestPeek(t *testing.T) {
-    s := New()
-    n := 15;
-    rand.Seed(time.Time{}.Unix())
+	s := New()
+	n := 15
+	rand.Seed(time.Time{}.Unix())
 
-    want := rand.Int()
-    for i := 0; i < n - 1; i++ {
-        s.Push(rand.Int())
-    }
-    s.Push(want)
+	want := rand.Int()
+	for i := 0; i < n-1; i++ {
+		s.Push(rand.Int())
+	}
+	s.Push(want)
 
-    if data, ok := s.Peek(); ok {
-        if *data != want {
-            t.Errorf("Peek: incorrect top: got %v, wanted %v", *data, want)
-        }
-    } else {
-        t.Errorf("Peek: invalid return status")
-    }
+	if data, ok := s.Peek(); ok {
+		if *data != want {
+			t.Errorf("Peek: incorrect top: got %v, wanted %v", *data, want)
+		}
+	} else {
+		t.Errorf("Peek: invalid return status")
+	}
 }
